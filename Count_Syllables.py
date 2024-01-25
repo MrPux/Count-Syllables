df1eb9a8ee899a7cc701f5783d0de33619c1dd60
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-25 13:14:15 -0500

    First commit with instructions

---
+++ Count_Syllables.py
@@ -0,0 +1,6 @@
+#Function named 'count' takes a single parameter. The paremeter is a string.
+#The string contains a single word devided into syllables by hyphens.
+
+#Function counts the number of syllables and returns it.
+    #E.g, count("ho-tel") returns 2
+
